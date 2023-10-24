63b05278896e51c7699d38dfb36407d84116e987
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-10-23 19:19:15 -0700

    Input DOM element

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Elements 
+const apiKey = "9cc3a5fdf9dc25bb9e98eef3b9bfc4f8"; // OpenWeather API key 
+const searchBtn = document.getElementById('searchBtn');
+const cityInput = document.getElementById('cityInput');
+const searchHistory = document.querySelector('.search-history');
+const currentWeather = document.querySelector('.current-weather');
+const forecast = document.querySelector('.forecast');
+
+
+
