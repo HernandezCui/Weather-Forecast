178c3c7d3d792ac939f4d4bf438f61398c18b7ec
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-11-09 17:42:52 -0800

    added gloval variables & apiKey

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// // DOM Elements 
-// const apiKey = "9cc3a5fdf9dc25bb9e98eef3b9bfc4f8"; // OpenWeather API key 
-// const searchBtn = document.getElementById('searchBtn');
-// const cityInput = document.getElementById('cityInput');
-// const searchHistory = document.querySelector('.search-history');
-// const currentWeather = document.querySelector('.current-weather');
-// const forecast = document.querySelector('.forecast');
+$(document).ready(function() {
+
+// API Key & Global Variables 
+const apiKey = "9cc3a5fdf9dc25bb9e98eef3b9bfc4f8";
+const searchForm = $('#search-form');
+const cityInput = $('#cityInput');
+const searchHistory = $('#searchedCity');
+
+
+
+
+
+
+
+
+
+});
 
 
 
