aa244a10fa4aad97a4baed89507be213a4056586
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-11-18 17:32:44 -0800

    event listenr for search button

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// API keys and URL 
+
+const apiKey = "";
+const apiUrl = "";
+
+// Event listener for Search button 
+document.getElementById("searchbtn").addEventListener("click", function (event) {
+    event.preventDefault();
+
+
+
+});
+
 
 
 
